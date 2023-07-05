da6f0b51af854587fd1cb7a37c43497bb7830a26
Vishwajeet <vishwajeetraj921@gmail.com>
2023-07-05 17:12:12 +0530

    String indexing and length of string

---
+++ file02.js
@@ -0,0 +1,13 @@
+// String Indexing :
+
+let firstName = "harshit";
+console.log(firstName[6]);
+
+// h a r s h i t
+// 0 1 2 3 4 5 6
+
+
+// length of a string :
+console.log(firstName[firstName.length - 1]);
+
+// Last Index : Length - 1;

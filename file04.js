c2ed637e8bf8f59972f6dd647d528380db1af9f2
Vishwajeet <vishwajeetraj921@gmail.com>
2023-07-06 11:09:27 +0530

    Different data types and conversions

---
+++ file04.js
@@ -0,0 +1,39 @@
+// (primitive)data types :
+// string - 'harshit'
+// number - 2 4 6 7
+// booleans
+// undefined
+// null
+// BigInt
+// symbol
+
+// typeof operator
+
+let age = 22;
+let firstName = 'Amit';
+console.log(typeof age);
+console.log(typeof firstName);
+console.log(typeof 'hasrshit');
+console.log(typeof 2789);
+
+// convert number to string
+
+age = age + "";
+console.log(typeof age);
+
+let roll_number = 27;
+let new_roll_number = String(roll_number);
+console.log(typeof new_roll_number);
+
+// convert string to number
+
+age = +age;
+console.log(typeof age);
+
+let form_no = "27";
+let new_form_no = Number(form_no);
+console.log(typeof new_form_no);
+
+
+
+

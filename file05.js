ef751c47945551cf5c74923d95c762dab703c6e7
Vishwajeet <vishwajeetraj921@gmail.com>
2023-07-07 13:41:08 +0530

    String concatenation

---
+++ file05.js
@@ -0,0 +1,14 @@
+// String concatenation : 
+
+let string1 = "Michael";
+let string2 = "Duke";
+
+let fullName = string1 + " " + string2;
+
+console.log(fullName);
+
+let string3 = "17";
+let string4 = "10";
+
+let newString = +string3 + +string4;
+console.log(typeof newString, newString);

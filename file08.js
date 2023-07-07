918a5dd8da4cff353395853dbebb897bd19f817f
Vishwajeet <vishwajeetraj921@gmail.com>
2023-07-07 18:22:51 +0530

    booleans and comparison operators

---
+++ file08.js
@@ -0,0 +1,17 @@
+// booleans and comparison operator :
+// booleans : true or false :
+
+let num1 = "7";
+let num2 = 7;
+console.log(num1 <= num2);
+
+// == vs === :
+
+console.log(num1 == num2); // checks only value.
+console.log(num1 === num2); // checks value as well as data type also.
+
+// != vs !== :
+
+console.log(num1 != num2);
+console.log(num1  !== num2);
+

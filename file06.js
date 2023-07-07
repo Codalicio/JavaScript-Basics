cd27b9a81aef93c71b9f542ca0d82c206153f89d
Vishwajeet <vishwajeetraj921@gmail.com>
2023-07-07 13:48:28 +0530

    Template String

---
+++ file06.js
@@ -0,0 +1,12 @@
+// Template string
+
+let age = 22;
+let firstName = "harshit";
+
+// "My name is harshit and my age is 22."
+
+// let aboutMe = "My name is " + firstName + " and my age is " + age + ".";
+// console.log(aboutMe);
+
+let aboutMe = `My name is ${firstName} and my age is ${age}.`;
+console.log(aboutMe);
